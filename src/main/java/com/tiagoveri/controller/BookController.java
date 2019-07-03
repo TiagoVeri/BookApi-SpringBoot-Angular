2b72a4f264e3c8f55ec1ca26cf3400e3260bab4f
TiagoVeri <tiagoverisjc@gmail.com>
2019-07-03 14:41:26 -0300

    Added CrossOrigin to accept Angular

--- src/main/java/com/tiagoveri/controller/BookController.java
+++ src/main/java/com/tiagoveri/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.tiagoveri.model.Book;
 import com.tiagoveri.service.BookService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value= "/books")
 public class BookController {
